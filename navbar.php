e4557718b1292a6987c43af2a51113c7f318e8ad
kinoppol <kinoppol@hotmail.com>
2023-01-12 11:57:43 +0700

    nav

--- navbar.php
+++ navbar.php
@@ -3,13 +3,18 @@
   <div class="container-fluid">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link active" href="demo.php">Home</a>
+        <a class="nav-link active" href="index.php">Home</a>
       </li>
 
       <li class="nav-item">
-        <a class="nav-link disabled" href="index.php">ShowStudent</a>
+        <a class="nav-link disabled" href="province.php">province</a>
       </li>
-
+      <li class="nav-item">
+        <a class="nav-link disabled" href="customer.php">customer</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="minor.php">minor.php</a>
+      </li>
     </ul>
   </div>
 </nav>
