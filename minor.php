71eb95e848d273f0e0d4373d9e7e850970d449a0
GolfZN <teerdach07@gfmail.com>
2023-01-12 12:11:25 +0700

    ES_DAY5

---
+++ minor.php
@@ -0,0 +1,53 @@
+<?php
+    include "header.php";
+    include "banner.php";
+    include "navbar.php";
+    include "conn.php";
+?>
+
+
+<div class="container mt-5">
+ 
+<table class="table table-dark table-hover table table-striped table-bordered table-hover table-success" style="font-family:cursive;">
+    <tr>
+      <th>MINOR_ID</th>
+      <th>MINOR_NAME</th>
+      <th>DEPART_NAME</th>
+    </tr>
+
+  <!-- LOOP FOR SELECT DATA FROM DATABASE -->
+<?php
+$sql = "SELECT * FROM minor";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+?>
+
+<tr>
+      <td><?php  echo $row["minor_id"] ?></td>
+      <td><?php  echo $row["minor_name"] ?></td>
+      <td><?php  echo $row["depart_name"] ?></td>
+    </tr>
+
+<?php
+  }
+} else {
+  echo "0 results";
+}
+
+mysqli_close($conn);
+?>
+</table>
+
+</div>
+
+
+</body>
+
+<?php
+    include "footer.php";
+?>
+
+</html>
