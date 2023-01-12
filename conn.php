c4c1f5afd87e99b6f5b9b4e2ca17022b5dca2ed9
kinoppol <kinoppol@hotmail.com>
2023-01-12 11:52:34 +0700

    conn

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "12345678";
-$dbname = "web2111";
+$dbname = "province";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
